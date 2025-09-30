ccca6ce9a04c18a0d59b2f2aa9fb844dae3479fb
Christian Westrom <c.westrom@westrom.xyz>
2025-09-30 10:18:40 +0900

    replace hol with the one from nixpkgs PR

--- pkgs/hol4/default.nix
+++ pkgs/hol4/default.nix
@@ -2,21 +2,33 @@
   lib,
   stdenv,
   pkgs,
-  fetchFromGitHub,
+  fetchurl,
   graphviz,
   fontconfig,
   liberation_ttf,
-  experimentalKernel ? true,
+  mlton,
+  experimentalKernel ? false,
 }:
 
 let
   pname = "hol4";
   vnum = "2";
 
+  version = "t.${vnum}";
   longVersion = "trindemossen-${vnum}";
-  version = longVersion;
   holsubdir = "hol-${longVersion}";
   kernelFlag = if experimentalKernel then "--expk" else "--stdknl";
+  buildSys = stdenv.buildPlatform.system;
+  hostSys = stdenv.hostPlatform.system;
+  # the current version of mlton doesn't support i686-linux
+  # it also fails to build with musl
+  useMlton =
+    buildSys != "i686-linux"
+    && hostSys != "i686-linux"
+    && !stdenv.buildPlatform.isMusl
+    && !stdenv.hostPlatform.isMusl
+    && lib.elem buildSys mlton.meta.platforms
+    && lib.elem hostSys mlton.meta.platforms;
 
   polymlEnableShared =
     with pkgs;
@@ -28,11 +40,9 @@ in
 stdenv.mkDerivation {
   name = "${pname}-${version}";
 
-  src = fetchFromGitHub {
-    owner = "HOL-Theorem-Prover";
-    repo = "HOL";
-    rev = longVersion;
-    sha256 = "sha256-esgqSlQ4M2DLNu02aALetuPJYsV/1RSEv65tEeVNno0=";
+  src = fetchurl {
+    url = "https://github.com/HOL-Theorem-Prover/HOL/releases/download/${longVersion}/${holsubdir}.tar.gz";
+    hash = "sha256-Ciy6IaB7LqwKlZOnEw1H1IcVoSL/bfbQxoWPcZD3H3w=";
   };
 
   buildInputs = [
@@ -40,9 +50,12 @@ stdenv.mkDerivation {
     graphviz
     fontconfig
     liberation_ttf
-  ];
+  ]
+  ++ lib.optional useMlton mlton;
 
-  buildCommand = ''
+  enableParallelBuilding = true;
+
+  buildPhase = ''
     mkdir chroot-fontconfig
     cat ${fontconfig.out}/etc/fonts/fonts.conf > chroot-fontconfig/fonts.conf
     sed -e 's@</fontconfig>@@' -i chroot-fontconfig/fonts.conf
@@ -52,45 +65,29 @@ stdenv.mkDerivation {
     export FONTCONFIG_FILE=$(pwd)/chroot-fontconfig/fonts.conf
 
     mkdir -p "$out/src"
-    cp -r "$src" "$out/src/${holsubdir}"
-    chmod -R u+w "$out/src/${holsubdir}" || true
-    cd "$out/src/${holsubdir}"
 
-    substituteInPlace tools/Holmake/Holmake_types.sml \
-      --replace "\"/bin/" "\"" \
+    cp -a . "$out/src"
+    cd "$out/src"
 
-
-    for f in tools/buildutils.sml help/src-sml/DOT;
-    do
-      substituteInPlace $f --replace "\"/usr/bin/dot\"" "\"${graphviz}/bin/dot\""
-    done
-
-    #sed -i -e "/compute/,999 d" tools/build-sequence # for testing
-
-    export HOLDIR="$PWD/"
     poly < tools/smart-configure.sml
 
-    # Provide wrappers expected by build system one level up from HOLDIR
-    WRAPDIR="$(dirname "$PWD")"
-    for WRAP in HOLlinkToSigobj ${holsubdir}linkToSigobj; do
-      cat > "$WRAPDIR/$WRAP" <<'EOF'
-    #!/bin/sh
-    set -e
-    DIR="$(dirname "$0")/${holsubdir}"
-    exec "$DIR/bin/linkToSigobj" "$@"
-    EOF
-      chmod +x "$WRAPDIR/$WRAP"
-    done
+    jobs=''${enableParallelBuilding:+$NIX_BUILD_CORES}
 
-    bin/build ${kernelFlag}
+    # Extra theories we want to build
+    echo 'examples/formal-languages/context-free' >> tools/sequences/final-examples
 
+    # We run `bin/build` twice to force HOL to generate `.hol/make-deps/*Theory.{sml,sig}.d` files
+    # See https://github.com/HOL-Theorem-Prover/HOL/issues/1670 for more info
+    bin/build ${kernelFlag} -j ''${jobs:-1}
+    bin/build ${kernelFlag} -j ''${jobs:-1}
+  '';
+
+  installPhase = ''
     mkdir -p "$out/bin"
-    ln -st $out/bin $out/src/${holsubdir}/bin/*
-    # ln -s $out/src/hol4.${version}/bin $out/bin
+    ln -st "$out/bin" "$out"/src/bin/*
   '';
 
   meta = with lib; {
-    broken = (stdenv.hostPlatform.isLinux && stdenv.hostPlatform.isAarch64);
     description = "Interactive theorem prover based on Higher-Order Logic";
     longDescription = ''
       HOL4 is the latest version of the HOL interactive proof
@@ -104,7 +101,7 @@ stdenv.mkDerivation {
       implementing combinations of deduction, execution and property
       checking.
     '';
-    homepage = "http://hol.sourceforge.net/";
+    homepage = "https://hol-theorem-prover.org/";
     license = licenses.bsd3;
     platforms = platforms.unix;
     maintainers = with maintainers; [ mudri ];
